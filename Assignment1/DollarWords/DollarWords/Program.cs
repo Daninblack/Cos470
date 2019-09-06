137556f41b19d40c55fbf8d9c8ae774006bd3d57
Daniel Fonseca <daniel.fonseca@maine.edu>
2019-09-05 22:26:28 -0400

    First initial code

---
+++ Assignment1/DollarWords/DollarWords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DollarWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
