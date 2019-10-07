fd718b3aabb09666c5690f6a1fe930433c5c351e
Daniel Fonseca <daniel.fonseca@maine.edu>
2019-10-06 22:16:13 -0400

    Done

---
+++ Assignment3/HaveWeMet/HaveWeMet/LocationHistoryAnalysis.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HaveWeMet
+{
+    public class LocationHistoryAnalysis
+    {
+        /*
+        * Checks where you were on a particular day
+        * Returns first instance found
+        */
+        public static bool CheckAlibi(DateTime date, LocationHistory locationHistory)
+        {
+            foreach (var location in locationHistory.locations)
+            {
+                var day = LocationHistoryHelperMethods.UnixTimeStampToDateTime(location.timestampMs);
+                if (DateTime.Compare(date, day) == 0)
+                {
+                    Console.WriteLine("Location:\n\nLatitude: " + location.latitudeE7 + "\tLongitude: " + location.longitudeE7);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool HaveWeMet(LocationHistory locationHistory1, LocationHistory locationHistory2)
+        {
+            for (int i = 0; i < locationHistory1.locations.Count; i++)
+            {
+                DateTime date1 = LocationHistoryHelperMethods.UnixTimeStampToDateTime(locationHistory1.locations[i].timestampMs);
+                for (int j = 0; j < locationHistory2.locations.Count; j++)
+                {
+                    DateTime date2 = LocationHistoryHelperMethods.UnixTimeStampToDateTime(locationHistory2.locations[j].timestampMs);
+                    //Checks if the time is relatively close
+                    if (LocationHistoryHelperMethods.DateTimesCoincide(date1, date2) == true)
+                    {
+                        var lat1 = locationHistory1.locations[i].latitudeE7;
+                        var lon1 = locationHistory1.locations[i].longitudeE7;
+                        var lat2 = locationHistory2.locations[j].latitudeE7;
+                        var lon2 = locationHistory2.locations[j].longitudeE7;
+                        var HaveWeMet = LocationHistoryHelperMethods.LocationsCoincide(lat1, lon1, lat2, lon2);
+                        //Checks if the coordinates are relatively close
+                        if (HaveWeMet == true)
+                        {
+                            Console.WriteLine("We have met!\n\nLatitude: " + lat1 + "\tLongitude: " + lon1);
+                            Console.WriteLine("\nDate: " + date1 + "\tUnix Time: " + locationHistory1.locations[i].timestampMs);
+                            return true;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("We have not met before.");
+            return false;
+        }
+
+
+    }
+}
---
+++ Assignment3/HaveWeMet/HaveWeMet/LocationHistoryHelperMethods.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HaveWeMet
+{
+    public class LocationHistoryHelperMethods
+    {
+        /*
+         * Constructs the JSON given a text file
+         */
+        public static string BuildJSONFromFile(string filePath)
+        {
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            StringBuilder json = new StringBuilder();
+            foreach (string line in lines)
+            {
+                json.AppendLine(line);
+            }
+
+            return json.ToString();
+        }
+
+        /*
+         * Deserializes a json file
+         * Returns a LocationHistory
+         */
+        public static LocationHistory DeserializeJSON(string json)
+        {
+            try
+            {
+                var locationHistory = JsonConvert.DeserializeObject<LocationHistory>(json);
+                return locationHistory;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("We had a problem: " + e.Message.ToString());
+                return null;
+            }
+        }
+
+        /*
+         * Takes a timestampMs string
+         * Returns a DateTime value that's comprehensible for humans
+         */
+        public static DateTime UnixTimeStampToDateTime(string timestampMs)
+        {
+            var date = (new DateTime(1970, 1, 1)).AddMilliseconds(double.Parse(timestampMs));
+            //Ignores milliseconds
+            var newDate = date.AddMilliseconds(-date.Millisecond);
+            return newDate;
+        }
+
+        /*
+         * Compares two DateTime instances
+         * Returns whether or not the times are close enough
+         * Times are close enough if they are 20 minutes apart or less
+         */
+        public static bool DateTimesCoincide(DateTime date1, DateTime date2)
+        {
+            double timeSpanLimit = 20;
+            TimeSpan timeSpan = date1 - date2;
+            double duration = Math.Abs(timeSpan.TotalMinutes);
+
+            if (duration <= timeSpanLimit)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /*
+         * Converts latitudeE7 and longitudeE7 into actual coordinates
+         */
+        public static double FormatCoordinate(double coordinate)
+        {
+            return (coordinate / 1e7);
+        }
+
+        /*
+         * Compares two coordinates and determines whether they are close enough
+         * Coordinates are close enough if they are 10 meters apart or less
+         */
+        public static bool LocationsCoincide(double latitude1, double longitude1, double latitude2, double longitude2)
+        {
+            double distanceLimit = 10.0;
+            double long1 = FormatCoordinate(longitude1);
+            double lat1 = FormatCoordinate(latitude1);
+            double long2 = FormatCoordinate(longitude2);
+            double lat2 = FormatCoordinate(latitude2);
+
+            var coordinate1 = new GeoCoordinate(lat1, long1);
+            var coordinate2 = new GeoCoordinate(lat2, long2);
+            double distance = coordinate1.GetDistanceTo(coordinate2);
+
+            if (distance <= distanceLimit)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
