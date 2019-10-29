0208f34eb3a474f17ddf4215d9f9502c3f393b6d
Daniel Fonseca <daniel.fonseca@maine.edu>
2019-10-29 11:48:32 -0400

    Created API. Returns Location History JSON for now

---
+++ Assignment3/HaveWeMet/WebApplication/Controllers/HaveWeMetController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HaveWeMet;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaveWeMetAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HaveWeMetController : ControllerBase
+    {
+        static Dictionary<int, LocationHistory> LocationHistories = new Dictionary<int, LocationHistory>();
+        string json;
+
+        public HaveWeMetController()
+        {
+            var filePath = @"C:\Users\Daniel\Desktop\Cos470 folder\Assignment3\HaveWeMet\WebApplication\LocationHistoryVeryShort.json";
+            json = LocationHistoryHelperMethods.BuildJSONFromFile(filePath);
+            var locationHistory = LocationHistoryHelperMethods.DeserializeJSON(json);
+            LocationHistories.Add(1, locationHistory);
+        }
+
+        // GET api/HaveWeMet
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return json;
+        }
+    }
+}
